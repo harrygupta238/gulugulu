84dfa48942b4c4b869eeb3b571f9e202a4f0901e
AadiVishu4 <aadivishu4@gmail.com>
2020-04-13 23:42:13 +0100

    logo modified

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 		
 		<div id="header_clr" class="container p-3 border text-center text-white">
 			<h1>
-			   <span class="text-light"><a id="gulugulu" href="index.php">Gulu <i class="fas fa-comments"></i> Gulu</a></span>
+			   <span class="text-light"><a id="gulugulu" href="index.php">Gulu<i class="fas fa-comments"></i>Gulu</a></span>
             </h1>
 			<p style="font-size: 14px;">
 				Protect your identity, <span id="header_cont">Send messages to your friends secretly.</span>
