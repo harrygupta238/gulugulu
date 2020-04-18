3f90368a3794d72fce4c1f93f11cc66d3bfa789b
AadiVishu4 <aadivishu4@gmail.com>
2020-04-18 18:22:33 +0100

    vistorprofilelink modeified and the  css updated.

--- profilelink.php
+++ profilelink.php
@@ -13,34 +13,35 @@ if($res=="-")
 		include("header.php"); 
 
  ?>
-		<div class="container bg-dark p-3 border-top text-white text-center">
+		<div class="profilelink_page container p-3 border-top text-center">
 			<div class="container">
 				<div class="row d-flex justify-content-center">
 					<div class=" col-lg-8 col-xl-8 col-sm-12 col-md-12">
-						<h4>
+						<!-- <h4>
 						Gulu-Gulu is a platform, where people can share their
 						feelings with their friends secretly.
-						</h4>
+						</h4> -->
 						<p>
 							<!-- <button type="button" class="btn btn-outline-danger btnplinkhome">
 								Signup
 							</button> -->
-							<button type="button" class="btn btn-outline-danger btnplinkhome">
+							<button type="button" class="btn btn-primary btnplinkhome btn-sm">
 							<span class="fas fa-home"></span>	Home
 							</button>
-						</p>
-					<p>
+						</p>  
+          
+					<p class="text-secondary">
 						So, what are you waiting for, it's time to do some  <span class="text-danger">Gulu-Gulu</span> with your loved ones.
 					</p>
-					<p>
+					<p class="text-secondary">
 						Don't worry your friend will not be able to see who shared this message to him/her.
 					</p>
-					<p>Your message will be sent to : <span class="text-info"><?php echo $un; ?></span></p>	
+					<p class="text-secondary">Your message will be sent to : <span class="text-success"><?php echo $un; ?></span></p>	
 					</div>
 				</div>
 			</div>
 		</div>
-			<div class="container bg-dark p-3 border-bottom text-white">
+			<div class="profilelink_page container p-3 border-bottom">
 				<div class="container">
 				<form action="#" class="needs-validation" id="sndmsgForm" method="post">
 					<div class="row d-flex justify-content-center">
@@ -61,21 +62,21 @@ if($res=="-")
 					</div>
 					<div class="row d-flex justify-content-center">
 						<div class="form-group col-lg-8 col-xl-8 col-sm-12 col-md-12">
-						<label for="pwd">Message:</label>
+						<!-- <label for="pwd">Message:</label> -->
 						 <textarea class="form-control message" rows="5" id="comment" placeholder="Write the message you want to share.."></textarea>
 						
 					</div>					
 					</div>
 					<div class="row d-flex justify-content-center text-center">
 						<div class="form-group col-lg-4 col-xl-4 col-sm-4 col-md-4">
-							<button type="submit" class="btn btn-danger btnMsgSubmit">
+							<button type="submit" class="btn btn-danger btnMsgSubmit btn-sm">
 								<span class="fas fa-paper-plane"></span> Send
 							</button>
 						</div>
 					</div>
 				</form>
 			</div>
-			<div
+			<!-- <div
 				class="container bg-dark p-3 my-3  text-center text-white"
 				style="font-family: 'Fredoka One';"
 			>
@@ -94,13 +95,9 @@ if($res=="-")
 					</li>
 				</ul>
 			</div>
-			</div>
+			</div> -->
 								
-			<div class="container bg-dark border p-3 text-white text-center">
-				<p>
-					Copyright &copy; 2020 All Rights Reserved by Spot<span class="text-danger">Chitt</span>
-				</p>
-			</div>
+			<?php include("footer.php");  ?>
 			<script type="text/javascript" src="js/controls.js"></script>
 			
 	</body>
